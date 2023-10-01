9b2e402b6454f49a51996cba8ce0fdfc2c708324
Víctor Vidal V <32011918+victorvidalv@users.noreply.github.com>
2023-10-01 09:40:24 -0300

    Update HomeModern.vue

--- components/HomeModern.vue
+++ components/HomeModern.vue
@@ -1,16 +1,16 @@
 <template>
-    <!-- Features -->
+    <!--  -->
 <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-  <!-- Grid -->
+  <!--  -->
   <div class="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
     <div>
       <img  src="https://dummyimage.com/580x580/eee/aaa" alt="Image Description">
     </div>
-    <!-- End Col -->
+    <!--  -->
 
     <div class="mt-5 sm:mt-10 lg:mt-0">
       <div class="space-y-6 sm:space-y-8">
-        <!-- Title -->
+        <!--  -->
         <div class="space-y-2 md:space-y-4">
           <h2 class="font-bold text-3xl lg:text-4xl text-gray-800">
             Innovación y Precisión en Servicios Contables
@@ -20,17 +20,17 @@
 
           </p>
         </div>
-        <!-- End Title -->
+        <!-- -->
 
         <!-- List -->
         <ul role="list" class="space-y-2 sm:space-y-4">
           <li class="flex space-x-3">
-            <!-- Solid Check -->
+            <!-- -->
             <svg class="flex-shrink-0 h-6 w-6 text-blue-600 " width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M15.1965 7.85999C15.1965 3.71785 11.8387 0.359985 7.69653 0.359985C3.5544 0.359985 0.196533 3.71785 0.196533 7.85999C0.196533 12.0021 3.5544 15.36 7.69653 15.36C11.8387 15.36 15.1965 12.0021 15.1965 7.85999Z" fill="currentColor" fill-opacity="0.1"/>
               <path d="M10.9295 4.88618C11.1083 4.67577 11.4238 4.65019 11.6343 4.82904C11.8446 5.00788 11.8702 5.32343 11.6914 5.53383L7.44139 10.5338C7.25974 10.7475 6.93787 10.77 6.72825 10.5837L4.47825 8.5837C4.27186 8.40024 4.25327 8.0842 4.43673 7.87781C4.62019 7.67142 4.93622 7.65283 5.14261 7.83629L7.01053 9.49669L10.9295 4.88618Z" fill="currentColor"/>
             </svg>
-            <!-- End Solid Check -->
+            <!--  -->
 
             <span class="text-sm sm:text-base text-gray-500">
               <span class="font-bold"> Especialización </span>   en Sectores Locales
@@ -38,12 +38,12 @@
           </li>
 
           <li class="flex space-x-3">
-            <!-- Solid Check -->
+            <!-- -->
             <svg class="flex-shrink-0 h-6 w-6 text-blue-600" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M15.1965 7.85999C15.1965 3.71785 11.8387 0.359985 7.69653 0.359985C3.5544 0.359985 0.196533 3.71785 0.196533 7.85999C0.196533 12.0021 3.5544 15.36 7.69653 15.36C11.8387 15.36 15.1965 12.0021 15.1965 7.85999Z" fill="currentColor" fill-opacity="0.1"/>
               <path d="M10.9295 4.88618C11.1083 4.67577 11.4238 4.65019 11.6343 4.82904C11.8446 5.00788 11.8702 5.32343 11.6914 5.53383L7.44139 10.5338C7.25974 10.7475 6.93787 10.77 6.72825 10.5837L4.47825 8.5837C4.27186 8.40024 4.25327 8.0842 4.43673 7.87781C4.62019 7.67142 4.93622 7.65283 5.14261 7.83629L7.01053 9.49669L10.9295 4.88618Z" fill="currentColor"/>
             </svg>
-            <!-- End Solid Check -->
+            <!-- -->
 
             <span class="text-sm sm:text-base text-gray-500">
                 Enfoque <span class="font-bold">Multidisciplinario </span>
@@ -51,24 +51,24 @@
           </li>
 
           <li class="flex space-x-3">
-            <!-- Solid Check -->
+            <!--  -->
             <svg class="flex-shrink-0 h-6 w-6 text-blue-600" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M15.1965 7.85999C15.1965 3.71785 11.8387 0.359985 7.69653 0.359985C3.5544 0.359985 0.196533 3.71785 0.196533 7.85999C0.196533 12.0021 3.5544 15.36 7.69653 15.36C11.8387 15.36 15.1965 12.0021 15.1965 7.85999Z" fill="currentColor" fill-opacity="0.1"/>
               <path d="M10.9295 4.88618C11.1083 4.67577 11.4238 4.65019 11.6343 4.82904C11.8446 5.00788 11.8702 5.32343 11.6914 5.53383L7.44139 10.5338C7.25974 10.7475 6.93787 10.77 6.72825 10.5837L4.47825 8.5837C4.27186 8.40024 4.25327 8.0842 4.43673 7.87781C4.62019 7.67142 4.93622 7.65283 5.14261 7.83629L7.01053 9.49669L10.9295 4.88618Z" fill="currentColor"/>
             </svg>
-            <!-- End Solid Check -->
+     
 
             <span class="text-sm sm:text-base text-gray-500">
                 Uso Intensivo de Tecnología y Análisis de Datos
             </span>
           </li>
         </ul>
-        <!-- End List -->
+        <!--  -->
       </div>
     </div>
-    <!-- End Col -->
+    <!--  -->
   </div>
-  <!-- End Grid -->
+   <!--  -->
 </div>
-<!-- End Features -->
+<!--  -->
 </template>
