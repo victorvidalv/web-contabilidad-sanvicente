89e49524d4ab888039b2f4daf6b5118e61dffcf3
Víctor Vidal V <32011918+victorvidalv@users.noreply.github.com>
2023-10-01 09:38:28 -0300

    Update ServiceSingleDetails.vue

--- components/ServiceSingleDetails.vue
+++ components/ServiceSingleDetails.vue
@@ -1,13 +1,13 @@
 <template><div class="bg-details">
-    <!-- Features -->
+    <!--  -->
     <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto ">
-      <!-- Grid -->
+      <!--  -->
       <div class="md:grid md:grid-cols-6 md:items-center md:gap-6 xl:gap-6">
         
     
         <div class="mt-5 sm:mt-10 lg:mt-0 col-span-4">
           <div class="space-y-6 sm:space-y-8">
-            <!-- Title -->
+            <!--  -->
             <div class="space-y-2 md:space-y-4">
               <h2 class="font-bold text-3xl lg:text-4xl text-gray-800 ">
                 Detalles del Servicio
@@ -16,7 +16,7 @@
        
               </p>
             </div>
-            <!-- End Title -->
+            <!--  -->
     
           </div>
         </div>
@@ -24,12 +24,12 @@
         <div class="col-span-2">
           <img  src="https://dummyimage.com/410x550/eee/aaa" alt="Image Description">
         </div>
-        <!-- End Col -->
-        <!-- End Col -->
+        <!--  -->
+        <!--  -->
       </div>
-      <!-- End Grid -->
+      <!--  -->
     </div>
-    <!-- End Features -->
+    <!--  -->
     
     
     </div>
