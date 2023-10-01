4feb49c5f828959fcf0104d0948b2258efd1140d
Víctor Vidal V <32011918+victorvidalv@users.noreply.github.com>
2023-10-01 09:35:23 -0300

    Update TrainCards.vue

--- components/TrainCards.vue
+++ components/TrainCards.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- Card Blog -->
+    <!-- Componente-->
     <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <!-- Título -->
         <div class="py-10 text-center">
