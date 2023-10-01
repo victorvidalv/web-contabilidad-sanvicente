a6005fb20eafbc0b08c097f31a9ad0cc74577c26
Víctor Vidal V <32011918+victorvidalv@users.noreply.github.com>
2023-10-01 09:47:29 -0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -30,7 +30,7 @@
             content: "Ofrecemos una amplia gama de servicios contables y empresariales en San Vicente de Tagua Tagua. Explore nuestras soluciones y descubra cómo podemos ayudarle a prosperar.", // Contenido de la etiqueta meta
           },
           {
-            hid: "keywords", // Identificador único para esta etiqueta meta
+            hid: "keywords", // Identificador  para esta etiqueta meta
             name: "keywords", // Nombre de la etiqueta meta
             content: "Servicios Integrales, San Vicente de Tagua Tagua, Bienvenida, Soluciones Contables, Asesoramiento Contable", // Contenido de la etiqueta meta
           },
