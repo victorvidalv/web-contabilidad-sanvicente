793f00eb665d26d2a6f7b6a1727844c7df59c6e5
Víctor Vidal V <32011918+victorvidalv@users.noreply.github.com>
2023-11-23 11:22:46 +0000

    fix client ( movil version)

--- components/SiteClients.vue
+++ components/SiteClients.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Sección de Clientes -->
-    <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto" >
+    <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <!-- Título y Descripción -->
         <div class="py-10 text-center">
             <h2 class="text-2xl font-bold md:text-4xl md:leading-tight text-gray-600">
@@ -13,11 +13,12 @@
         </div>
 
         <!-- Lista de Logos de Clientes -->
-        <div class="grid grid-cols-2 sm:grid-cols-3 sm:grid-cols-4 md:grid-cols-4 gap-3 lg:gap-6">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3 lg:gap-6">
             <!-- Utilizamos un bucle 'v-for' para renderizar los logos -->
             <div v-for="i in 8" :key="i">
                 <!-- Aplicamos clases dinámicas para los logos -->
                 <div :class="`border clients_logo-${i}`">
+                    <!-- Aquí iría el contenido del logo, como una imagen o similar -->
                 </div>
             </div>
         </div>
