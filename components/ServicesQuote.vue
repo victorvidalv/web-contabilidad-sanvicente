d401b5ac539977e99801cd909e64b1646e671e61
Víctor Vidal V <32011918+victorvidalv@users.noreply.github.com>
2024-03-07 12:59:39 -0300

    add representation

--- components/ServicesQuote.vue
+++ components/ServicesQuote.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-10 bg-gray-200 gap-4" :id="id">
+    <div class="grid grid-cols-1 md:grid-cols-10 border  gap-4 bg-gray-200 min-h-40" >
       <!-- Oculta en pantallas menores a 'md' y muestra en 'md' o más grandes -->
      
       <!-- Ajusta el span para ocupar todo el ancho en pantallas pequeñas y 7/10 en pantallas md o mayores -->
@@ -16,14 +16,8 @@
 export default {
 
     props: {
-        id: {
-            type: String,
-            required: true
-        },
-        img: {
-            type: String,
-            required: true
-        }
+      
+        
     }
 
 }
