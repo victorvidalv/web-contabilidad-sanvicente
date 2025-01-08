aa91fe4f788af592a14cd1cb0fb76af1239cebd6
victor vidal <victorvidalv@gmail.com>
2025-01-08 15:43:58 -0300

    update tailwindcss version and modify SiteMenu links

--- components/SiteMenu.vue
+++ components/SiteMenu.vue
@@ -38,8 +38,7 @@
               exact-active-class="bg-orange-600 text-white" to="/capacitacion/">Capacitación</nuxt-link>
  
 
-              <nuxt-link class="font-medium md:w-32 md:text-center sm:py-6 py-2 px-2 "
-              exact-active-class="bg-orange-600 text-white" to="/blog/">Publicaciones</nuxt-link>
+          
    <nuxt-link class="font-medium md:w-32 md:text-center sm:py-6 py-2 px-2 "
                exact-active-class="bg-orange-600 text-white" to="/herramientas/">Herramientas</nuxt-link>
   
